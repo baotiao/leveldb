c96a0748b76a3ed0d6dd30cca19d5755e308aa37
baotiao <baotiao@gmail.com>
2013-11-14 23:49:09 +0800

    commit -a -m the code of expire time

--- db/db_iter.cc
+++ db/db_iter.cc
@@ -257,8 +257,9 @@ void DBIter::Seek(const Slice& target) {
   direction_ = kForward;
   ClearSavedValue();
   saved_key_.clear();
+  time_t now = time(0);
   AppendInternalKey(
-      &saved_key_, ParsedInternalKey(target, sequence_, kValueTypeForSeek));
+      &saved_key_, ParsedInternalKey(target, ctime(&now), sequence_, kValueTypeForSeek));
   iter_->Seek(saved_key_);
   if (iter_->Valid()) {
     FindNextUserEntry(false, &saved_key_ /* temporary storage */);
--- db/version_set.cc
+++ db/version_set.cc
@@ -148,7 +148,9 @@ bool SomeFileOverlapsRange(
   uint32_t index = 0;
   if (smallest_user_key != NULL) {
     // Find the earliest possible internal key for smallest_user_key
-    InternalKey small(*smallest_user_key, kMaxSequenceNumber,kValueTypeForSeek);
+
+    time_t now = time(0);
+    InternalKey small(*smallest_user_key, ctime(&now), kMaxSequenceNumber,kValueTypeForSeek);
     index = FindFile(icmp, files, small.Encode());
   }
 
@@ -428,8 +430,10 @@ int Version::PickLevelForMemTableOutput(
   if (!OverlapInLevel(0, &smallest_user_key, &largest_user_key)) {
     // Push to next level if there is no overlap in next level,
     // and the #bytes overlapping in the level after that are limited.
-    InternalKey start(smallest_user_key, kMaxSequenceNumber, kValueTypeForSeek);
-    InternalKey limit(largest_user_key, 0, static_cast<ValueType>(0));
+
+    time_t now = time(0);
+    InternalKey start(smallest_user_key, ctime(&now), kMaxSequenceNumber, kValueTypeForSeek);
+    InternalKey limit(largest_user_key, ctime(&now), 0, static_cast<ValueType>(0));
     std::vector<FileMetaData*> overlaps;
     while (level < config::kMaxMemCompactLevel) {
       if (OverlapInLevel(level + 1, &smallest_user_key, &largest_user_key)) {
